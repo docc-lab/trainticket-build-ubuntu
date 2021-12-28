6e8459bcd3b304981a062f4c369230e4df4d4e87
Tomislav Žabčić-Matić <tome215@gmail.com>
2021-12-28 00:36:19 -0500

    basic test 001

---
+++ dockerSetup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker --no-pager
+sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+sudo echo -e '\n\nexport PATH=$PATH:/usr/local/bin' >> .bashrc
+source .bashrc
+
---
+++ profile.py
@@ -0,0 +1,24 @@
+"""
+Setup script for deploying train-ticket on CloudLab
+"""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="bash", command="/local/repository/changeShells.sh"))
+#node.addService(pg.Execute(shell="bash", command="/local/repository/dockerSetup.sh"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
